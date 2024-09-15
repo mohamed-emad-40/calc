1972ef3b37c3deac08713f36d7bf0b099b8f45e8
alomda <alomdam40@gmail.com>
2024-09-15 23:52:30 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const display = document.getElementById("display");
+
+function appendtodisplay(input) {
+  display.value += input;
+}
+function cleardisplay() {
+  display.value = "";
+}
+function calculate() {
+  display.value = eval(display.value);
+}
+console.log(Date());
